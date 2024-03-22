30d436d3918a8e9107eafe84c84b5164355015d7
mithridatem <mathieumithridate@adrar-formation.com>
2024-03-22 16:40:25 +0100

    feat : correction java adresse Swing GUI

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import com.SQLJava.Data.DBConnexion;
+import com.SQLJava.Data.User;
+import com.SQLJava.Data.UserManager;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        mainWindow mainWindow = new mainWindow();
+    }
+}
